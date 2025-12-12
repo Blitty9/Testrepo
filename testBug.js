126e9ce97277027d7fa94eddf79db7a2e8a6e9af
Blitty <bryanblitman@gmail.com>
2025-12-11 20:48:30 -0500

    BugSmith automated fix for issue #1

--- testBug.js
+++ testBug.js
@@ -3,26 +3,32 @@
 
 function addNumbers(a, b) {
     // BUG 1: Should add, but subtracts
-    return a - b
+    return a + b
 }
 
 // BUG 2: Missing semicolon & inconsistent formatting
-const message = "Hello from BugSmith"
+const message = "Hello from BugSmith";
 console.log(message)
 
 function fetchUser() {
     // BUG 3: Returns undefined instead of object
-    return
+    return {};
 }
 
 // BUG 4: loadData never returns the JSON data
 async function loadData() {
     try {
         const res = await fetch("https://jsonplaceholder.typicode.com/posts/1")
-        // Missing: return res.json()
-    } catch (err) {
-        console.error("Load error!", err)
+        return res.json();
+   } catch (err) {
+       console.error("Load error!", err)
+   }
     }
+
+module.exports = {
+   addNumbers,
+   fetchUser,
+   loadData
 }
 
 module.exports = {
